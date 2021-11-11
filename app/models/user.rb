6a74c2285bd357ab667fd4aaf77030f11ab3ff7e
Arthur Fincham <arthurfincham@me.com>
2021-11-11 17:11:21 +0000

    add associations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include ImageUploader::Attachment(:image)
-  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
+  has_many :likes
+  has_many :comments
 end
