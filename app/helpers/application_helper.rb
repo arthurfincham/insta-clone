14f626836fbbf6cc702cc5ff8ce2567abf1bf014
Arthur Fincham <arthurfincham@me.com>
2021-11-12 13:51:25 +0000

    button black if post already liked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,8 @@ end
   def id2ProfilePic(id)
     image_tag(User.find(id).image_url, class: "post-profile-pic rounded-circle mx-1")
   end
+
+  def already_liked(post)
+    Like.where(user_id: current_user.id, post_id: post.id).exists?
+  end
 end
