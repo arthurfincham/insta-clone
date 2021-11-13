79ad064efa710e83704011b91d5bb498428e1c40
Arthur Fincham <arthurfincham@me.com>
2021-11-13 15:52:45 +0000

    spec update for home controller

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,17 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Home controller", :type => :request do
-
-  before do
-    log_in_user
+  describe "before authentication" do
+    it "it redirects to auth without current user" do
+      get "/"
+      expect(response).to redirect_to("/users/sign_in")
+    end
   end
 
-  it "it redirects to auth without current user" do
-    get "/"
-    expect(response).to render_template(:index)
-  end
+  describe "after authentication" do
+    before do
+      log_in_user
+    end
 
+    it "renders index once authenticated" do
+      get "/"
+      expect(response).to render_template(:index)
+    end
+
+    it "has a successful http status" do
+      get "/"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the tag_page" do
+      get "/home/tag_page"
+      expect(response).to render_template(:tag_page)
+    end
+  end
 end
-
-
-
